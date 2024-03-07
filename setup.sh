29e70949caa0a2fc8537b48ace9d49dae861e218
Holla Dev <94074434+holladevio@users.noreply.github.com>
2024-03-07 11:07:24 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+# Запрос имени нового пользователя
+echo "Enter new user:"
+read username
+
+# Проверка, было ли введено имя пользователя
+if [ -z "$username" ]; then
+    echo "Имя пользователя не введено. Скрипт будет остановлен."
+    exit 1
+else
+    # Создание нового пользователя
+    sudo adduser "$username"
+fi
+
+
 # Обновление списка пакетов и установка обновлений
-sudo apt update && sudo apt upgrade -y
+# sudo apt update && sudo apt upgrade -y
 
 # Установка Nginx
 sudo apt install nginx -y
