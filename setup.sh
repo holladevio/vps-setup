919e18cf5e24d81e725d88fc1f31b6b113ec566c
Holla Dev <94074434+holladevio@users.noreply.github.com>
2024-03-08 14:33:45 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -21,17 +21,39 @@ fi
 sudo adduser "$username" sudo
 # groups "$username"
 
-# Установка Nginx
+# Install Nginx
 sudo apt install nginx -y
 sudo systemctl enable nginx
 sudo systemctl start nginx
 
-# Firewall
+# Setup Firewall
 sudo ufw allow "OpenSSH"
 sudo ufw allow 'Nginx HTTPS'
 sudo ufw enable
 # sudo ufw status
 
+# SSH
+rsync --archive —chown="$username":"$username" ~/.ssh /home/"$username"
+
+# Create Server Block
+sudo mkdir -p /var/www/"$domainname"/web
+sudo chown -R "$username":"$username" /var/www/"$domainname"
+sudo chmod -R 755 /var/www/"$domainname"
+
+# Setup Nginx for Server Block
+config_file="/etc/nginx/sites-available/$domainname"
+sudo touch "$config_file"
+echo "server {
+    listen 80;
+    server_name $domainname www.$domainname;
+
+    root /var/www/$domainname/html;
+    index index.html index.htm index.nginx-debian.html;
+
+    location / {
+        try_files \$uri \$uri/ =404;
+    }
+}" > "$config_file"
 
 # Обновление списка пакетов и установка обновлений
 # sudo apt update && sudo apt upgrade -y
